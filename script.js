1a82a0656a825ea281e4b9ad4a259ca47c0047f5
Meryem Sener <meryemaydinsener@gmail.com>
2019-12-29 22:01:06 +0100

    ,
    ,

--- script.js
+++ script.js
@@ -332,7 +332,7 @@ for (var i = 0; testArray.length; i++){   //i=i+1=i++
    for (var i = 0; i <this.todos.length; i++){
    console.log( this.todos[i].todoText); //this.todos kismi dizinde kac madde oldugunu, .todoText maddelerin textini de verir
    }
-  };
+  },
 
 
   // displayTodos should tell you if .todos is empty
